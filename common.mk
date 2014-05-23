ac5fa89d3a97bd3b9a136ad08a498403c4779a2a
Punit Soni <punits@quicinc.com>
2014-05-23 10:56:54 -0700

    fix issues and switch to new makefile
    - organized build tree in the out folder
    - link freeRTOS in the final binary

--- common.mk
+++ common.mk
@@ -12,9 +12,15 @@ OBJCOPY = arm-none-eabi-objcopy
 
 
 CPU = -mcpu=cortex-m3 -mthumb
+
 C_FLAGS = $(CPU) -c -g -fno-common -fmessage-length=0 -Wall \
           -fno-exceptions -ffunction-sections -fdata-sections 
+
 C_FLAGS += -MMD -MP
+
+LD_FLAGS = -mcpu=cortex-m3 -mthumb -Wl,--gc-sections --specs=nano.specs \
+-u _printf_float -u _scanf_float
+
 C_SYMBOLS = -DTARGET_NUCLEO_F103RB -DTARGET_M3 -DTARGET_STM \
             -DTOOLCHAIN_GCC_ARM -DTOOLCHAIN_GCC -D__CORTEX_M3 \
             -DARM_MATH_CM3 -DMBED_BUILD_TIMESTAMP=1400548239.15 -D__MBED__=1
